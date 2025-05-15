6a1d442e95988aea9d3fead77a33dfed96af63df
PopoGonry <rhtjdsh35@gmail.com>
2025-05-15 14:36:44 +0900

    2025-05-15
    1부터 100 사이의 숫자 입력 검사 프로그램

---
+++ 2025-05-15/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = 0;
+
+        while (true) {
+            System.out.print("1부터 100 사이의 숫자를 입력하세요: ");
+            num = scanner.nextInt();
+
+            if(1 <= num && num <= 100) {
+                System.out.println("입력 완료! 프로그램을 종료합니다.");
+                break;
+            }
+        }
+    }
+}
