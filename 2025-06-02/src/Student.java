9177ad03f9d5f60647e0cd5babb6cf7a6d11dd89
PopoGonry <rhtjdsh35@gmail.com>
2025-06-09 14:31:11 +0900

    2025-06-09

---
+++ 2025-06-02/src/Student.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    private String id;
+    private String name;
+    private String status;
+    private String birthdate;
+    private String contact;
+
+    private List<String> courses;
+
+    public Student() {
+        this.id = "";
+        this.name = "";
+        this.status = "";
+        this.birthdate = "";
+        this.contact = "";
+        courses = new ArrayList<>();
+    }
+
+    public Student(String id, String name, String status, String birthdate, String contact) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.birthdate = birthdate;
+        this.contact = contact;
+        courses = new ArrayList<>();
+    }
+
+    public void setStudent(String id, String name, String status, String birthdate, String contact) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.birthdate = birthdate;
+        this.contact = contact;
+        courses = new ArrayList<>();
+    }
+
+    public void printInfo() {
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("status = " + status);
+        System.out.println("birthdate = " + birthdate);
+        System.out.println("contact = " + contact);
+        printCourses();
+    }
+
+    public void addCourse(String course) {
+        courses.add(course);
+    }
+
+    public void printCourses() {
+        if(courses.isEmpty()) {
+            System.out.println("없음");
+            return;
+        }
+
+        for (String course : courses) {
+            System.out.println(course);
+        }
+
+    }
+}
